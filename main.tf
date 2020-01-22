fde096f3ae09638f2f2a643445a6aa728cb35ccb
Evan Wies <evan@neomantra.net>
2020-01-22 09:39:22 -0500

    add more variables and outputs, tag depends on sha

--- main.tf
+++ main.tf
@@ -35,6 +35,11 @@ variable "dest_prefix" {
   type        = string
 }
 
+variable "keep_locally" {
+  description = "If false, it will delete the image from the docker local storage on destroy operation."
+  default     = true
+}
+
 
 ###############################################################################
 # Locals
@@ -42,10 +47,10 @@ variable "dest_prefix" {
 
 locals {
   source      = "${var.source_prefix}${var.source_prefix == "" ? "" : "/"}"
-  dest        = "${var.dest_prefix}${var.dest_prefix == "" ? "" : "/"}"
   source_repo = "${local.source}${var.image_name}"
-  dest_repo   = "${local.dest}${var.image_name}"
   source_full = "${local.source_repo}:${var.image_tag}"
+  dest        = "${var.dest_prefix}${var.dest_prefix == "" ? "" : "/"}"
+  dest_repo   = "${local.dest}${var.image_name}"
   dest_full   = "${local.dest_repo}:${var.image_tag}"
 }
 
@@ -57,11 +62,39 @@ locals {
 output "dest_repo" {
   description = "Destination Repository without tag"
   value       = local.dest_repo
+  depends_on = [
+    docker_image.image.latest
+  ]
+}
+
+output "dest_full" {
+  description = "Full Destination image path as dest/image_name:image_tag"
+  value       = local.dest_full
+  depends_on = [
+    docker_image.image.latest
+  ]
+}
+
+output "dest_full_sha" {
+  description = "Full Destination image path as dest/image_name@sha256"
+  value       = "${local.dest_repo}@${data.docker_registry_image.source.sha256_digest}"
+  depends_on = [
+    docker_image.image.latest
+  ]
+}
+
+output "sha256_digest" {
+  description = "sha256 digest of the image"
+  value       = data.docker_registry_image.source.sha256_digest
+
 }
 
 output "tag" {
   description = "Image tag"
   value       = var.image_tag
+  depends_on = [
+    docker_image.image.latest
+  ]
 }
 
 
@@ -75,12 +108,12 @@ data "docker_registry_image" "source" {
 
 resource "docker_image" "image" {
   name          = data.docker_registry_image.source.name
+  keep_locally  = var.keep_locally
   pull_triggers = [data.docker_registry_image.source.sha256_digest]
-  keep_locally  = true
 
   provisioner "local-exec" {
     command = <<END_OF_COMMAND
-docker tag ${data.docker_registry_image.source.name} ${local.dest_full} && \
+docker tag ${replace(self.latest, "sha256:", "")} ${local.dest_full} && \
 docker push ${local.dest_full}
 END_OF_COMMAND
   }
